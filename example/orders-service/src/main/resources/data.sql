fd7e51216f5a6bb661cab4d64d8681cec306e20e
ar0ne <camtbitakou@yandex.ru>
2024-10-17 17:30:30 +0400

    renamed folder, added consumer module

---
+++ example/orders-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO "product" VALUES (default, 'Black Pen', 'Black ball pen');
+INSERT INTO "product" VALUES (default, 'Blue Pen', 'Blue gel pen');
+INSERT INTO "product" VALUES (default, 'Notepad', 'Notepad, 50 pages');
