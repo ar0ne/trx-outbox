6fbc0f35d734335844c1a055984884cb5089b8ab
ar0ne <camtbitakou@yandex.ru>
2024-10-18 19:11:44 +0400

    added liquibase

--- example/common/src/main/java/org/example/persistence/model/Product.java
+++ example/common/src/main/java/org/example/persistence/model/Product.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,6 +26,7 @@ public class Product {
     private Long id;
 
     @NotBlank
+    @NotNull
     private String name;
 
     private String description;
